10aa4a67e866f6b108a327418954ffd29974cb56
engageone <42962238+engageone@users.noreply.github.com>
2019-02-22 15:57:41 +0530

    Update en.js

--- language/en.js
+++ language/en.js
@@ -24,7 +24,7 @@ var Translations = {
 
     "PreparedForText": "Prepared for",
 
-    "CopyrightText": "Copyright " + Utils.getFullYear() + " Client Name",
+    "CopyrightText": "Copyright " + Utils.getFullYear() + " Rapids Business",
     "PoweredByFooterText": "Powered by",
     "HelpFooterText": "Help",
     "PrivacyPolicyFooterText": "Privacy Policy",
