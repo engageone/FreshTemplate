bad593151b681ad2c845017fedf4fca1f3b966d5
engageone <42962238+engageone@users.noreply.github.com>
2019-02-22 13:26:21 +0530

    Update Promos.js

--- tests/modules/Promos.js
+++ tests/modules/Promos.js
@@ -16,7 +16,7 @@
                     "trackingName": "promo1",
                     "url": "http://example.com/",
                     "label": "Promo (Top)",
-                    "desktopImage": "./images/promo_portrait.png",
+                    "desktopImage": "./images/wipro1.png",
                     "mobileImage": "./images/promo_landscape.png"
                 },
                 "promo2": {
