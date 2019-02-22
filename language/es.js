8900f5459b82771de29a60288473044f4b9ede50
engageone <42962238+engageone@users.noreply.github.com>
2019-02-22 19:17:37 +0530

    Update es.js

--- language/es.js
+++ language/es.js
@@ -43,7 +43,7 @@ var Translations = {
             "card": "7b480c70",
             "label": "Botón 2",
             "icon": "website",
-             "link":"href=http://www.wipro.com/"
+             "link":"http://www.wipro.com/"
         },
         "button3": {
             "card": "7b480c70",
