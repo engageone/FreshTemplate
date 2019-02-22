650ad5119e5090d0ca975492573fd0e19297d181
engageone <42962238+engageone@users.noreply.github.com>
2019-02-22 19:11:45 +0530

    Update CtaButtons.js

--- tests/modules/CtaButtons.js
+++ tests/modules/CtaButtons.js
@@ -12,14 +12,14 @@
                 "button1": {
                     "card": "d1b4d742",
                     "label": "Contact",
-                     "link": "href=http://www.wipro.com/en-IN/contact-wipro/",
+                     "url": "http://www.wipro.com/en-IN/contact-wipro/",
                     "icon": "contact"
                 },
                 "button2": {
                     "card": "d1b4d742",
                     "label": "Website",
                     "icon": "website",
-                    "link":"href=https://www.wipro.com/"
+                    "url":"https://www.wipro.com/"
                     
                 },
                 "button3": {
