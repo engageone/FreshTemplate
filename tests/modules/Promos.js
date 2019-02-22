819e85f2754f4e674a67a532cae2a83eaaa5d790
engageone <42962238+engageone@users.noreply.github.com>
2019-02-22 14:15:23 +0530

    Update Promos.js

--- tests/modules/Promos.js
+++ tests/modules/Promos.js
@@ -51,7 +51,7 @@
                 mobilePromoImages = $("#jsPromoContainerMobile .promo__image");
 
             assert.ok($(desktopPromoImages[0]).css("background-image").match(/img321\.png/) !== null);
-            assert.ok($(mobilePromoImages[0]).css("background-image").match(/promo_landscape\.png/) !== null);
+            assert.ok($(mobilePromoImages[0]).css("background-image").match(/img321\.png/) !== null);
             assert.ok($(desktopPromoImages[1]).css("background-image").match(/promo_portrait2\.png/) !== null);
             assert.ok($(mobilePromoImages[1]).css("background-image").match(/promo_landscape2\.png/) !== null);
 
